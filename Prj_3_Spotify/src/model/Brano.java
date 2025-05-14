cf24dc4e6a83c5b96859f85f1399a64ed1d664ca
DarioIEL <dario.mennillo@immaginazioneelavoro.it>
2025-05-14 09:29:15 +0200

    updato Spotify

--- Prj_3_Spotify/src/model/Brano.java
+++ Prj_3_Spotify/src/model/Brano.java
@@ -2,4 +2,14 @@ package model;
 
 public class Brano {
 
+	public String autore;
+	public double durata;
+
+	public Brano(String autore, double durata) {
+		this.autore = autore;
+		this.durata = durata;
+	}
+	
+	
+	
 }
