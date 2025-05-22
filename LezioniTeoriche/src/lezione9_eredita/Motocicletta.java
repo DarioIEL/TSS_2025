1b19acfd2de118b7e9c731db191451b0f5786c03
DarioIEL <dario.mennillo@immaginazioneelavoro.it>
2025-05-22 14:25:27 +0200

    update lezione 22/05

--- LezioniTeoriche/src/lezione9_eredita/Motocicletta.java
+++ LezioniTeoriche/src/lezione9_eredita/Motocicletta.java
@@ -3,9 +3,10 @@ package lezione9_eredita;
 public class Motocicletta extends Veicolo {
 
 	private String tipo;
+	private static int numRuote = 2;
 
 	public Motocicletta(String marca, String modello, int annoProd, String tipo) {
-		super(marca, modello, annoProd, 2);
+		super(marca, modello, annoProd, numRuote);
 		this.tipo = tipo;
 	}
 
